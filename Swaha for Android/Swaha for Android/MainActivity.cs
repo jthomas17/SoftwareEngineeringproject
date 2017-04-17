11a9142a118f3ade5e6bcd6daa1b4b67b12ca1f2
Ian Stenerson <istenerson17@my.whitworth.edu>
2017-04-17 13:45:37 -0700

    Initial Solution for Andriod Version of App
    Empty project for future development.

---
+++ Swaha for Android/Swaha for Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Swaha_for_Android
+{
+    [Activity(Label = "Swaha_for_Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
