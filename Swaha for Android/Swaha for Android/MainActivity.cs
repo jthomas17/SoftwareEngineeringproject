d907e33e3f7be55f69336851aa475c9c59fb8d07
Ian Stenerson <istenerson17@my.whitworth.edu>
2017-04-20 15:19:39 -0700

    Revert Main Activity
    Reverted the main activity back to basic functionality. Removed the code
    for camera access.

--- Swaha for Android/Swaha for Android/MainActivity.cs
+++ Swaha for Android/Swaha for Android/MainActivity.cs
@@ -18,82 +18,12 @@ namespace Swaha_for_Android
     [Activity(Label = "Swaha_for_Android", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        private ImageView _imageView;
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-
-            // Make it available in the gallery
-
-            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
-            Uri contentUri = Uri.FromFile(App._file);
-            mediaScanIntent.SetData(contentUri);
-            SendBroadcast(mediaScanIntent);
-
-            // Display in ImageView. We will resize the bitmap to fit the display.
-            // Loading the full sized image will consume to much memory
-            // and cause the application to crash.
-
-            int height = Resources.DisplayMetrics.HeightPixels;
-            int width = _imageView.Height;
-            App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
-            if (App.bitmap != null)
-            {
-                _imageView.SetImageBitmap(App.bitmap);
-                App.bitmap = null;
-            }
-
-            // Dispose of the Java side bitmap.
-            GC.Collect();
-        }
-        private void TakeAPicture(object sender, EventArgs eventArgs)
-        {
-            Intent intent = new Intent(MediaStore.ActionImageCapture);
-            App._file = new File(App._dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
-            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(App._file));
-            StartActivityForResult(intent, 0);
-        }
-
-        private void CreateDirectoryForPictures()
-        {
-            App._dir = new File(
-                Environment.GetExternalStoragePublicDirectory(
-                    Environment.DirectoryPictures), "Swaha_for_Android");
-            if (!App._dir.Exists())
-            {
-                App._dir.Mkdirs();
-            }
-        }
-
-        private bool IsThereAnAppToTakePictures()
-        {
-            Intent intent = new Intent(MediaStore.ActionImageCapture);
-            IList<ResolveInfo> availableActivities =
-                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
-            return availableActivities != null && availableActivities.Count > 0;
-        }
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-            if (IsThereAnAppToTakePictures())
-            {
-                CreateDirectoryForPictures();
-
-                Button button = FindViewById<Button>(Resource.Id.myButton);
-                _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
-                button.Click += TakeAPicture;
-            }
-            Button gotoPicSelect = FindViewById<Button>(Resource.Id.GoToPictureSelect);
-            gotoPicSelect.Click += delegate
-            {
-                var PicSelectIntent = new Intent(this, typeof(PicSelectAvtivity));
-                StartActivity(PicSelectIntent);
-            };
+            // SetContentView (Resource.Layout.Main);
         }
     }
 
