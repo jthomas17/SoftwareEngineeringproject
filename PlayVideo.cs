442467e405c6b7c880530ff4449160bde895ae8a
Emma Whitaker <ewhitaker17@my.whitworth.edu>
2017-05-03 16:15:43 -0700

    Add files via upload

---
+++ PlayVideo.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Swaha_for_Android
+{
+	[Activity(Label = "PlayVideo")]
+	public class PlayVideo : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			// Create your application here
+
+			SetContentView(Resource.Layout.PlayVideo);
+
+			//Button PlayButton 
+			//Button SaveVideo
+			//Button DiscardVideo
+
+
+			var videoView = FindViewById<VideoView>(XmlResourceParserReader.Id.SampleVideoView);
+
+			var uri = Android.Net.Uri.Parse (/* insert url*/)
+
+			videoView.SetVideoURI(uri);
+						videoView.Visibility = ViewStates.Visible;
+
+
+			videoView.Start();
+		}
+	}
+}
