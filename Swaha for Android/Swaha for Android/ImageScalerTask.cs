c86fba026539a5192709253a7e5b6f6f8ebc0ec5
j-tone <jtoney17@my.whitworth.edu>
2017-05-15 16:47:17 -0700

    Filling in some changes
    Added ImageScalerTask and updated PicSelectActivity AXML. Will update
    other classes later if needed.

---
+++ Swaha for Android/Swaha for Android/ImageScalerTask.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Graphics;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Swaha_for_Android
+{
+    class ImageScalerTask : AsyncTask<string, Java.Lang.Void, Bitmap>
+    {
+        private GridViewAdapter.ViewHolder holder;
+        private int pos;
+        // private readonly WeakReference<Bitmap> softRef;
+        public ImageScalerTask(GridViewAdapter.ViewHolder tile, int p)
+        {
+            holder = tile;
+            pos = p;
+        }
+
+        protected override Bitmap RunInBackground(params string[] @params)
+        {
+
+            if (holder.Position == pos)
+            {
+                return scaleBitmap(@params[0]);
+            }
+            else return null;
+        }
+
+        protected override void OnPostExecute(Bitmap result)
+        {
+            if (holder.Position == pos)
+            {
+                holder.Thumbnail.SetImageBitmap(result);
+            }
+        }
+
+        private Bitmap scaleBitmap(string address)
+        {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.InSampleSize = 8;
+            Bitmap bitmap = BitmapFactory.DecodeFile(address, options);
+
+            return bitmap;
+        }
+    }
+}
